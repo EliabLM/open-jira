96d1dee59cbb0ae7e931f6da5e274af81ee8f2f1
Eliab <eliablopez@hotmail.com>
2022-12-08 14:33:26 -0500

    Add Sidebar

---
+++ components/ui/Navbar.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from 'react';
+import { AppBar, Toolbar, IconButton, Typography } from '@mui/material';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+
+export const Navbar: FC = () => {
+  return (
+    <AppBar position='sticky' elevation={0}>
+      <Toolbar>
+        <IconButton size='large' edge='start'>
+          <MenuOutlinedIcon />
+        </IconButton>
+        <Typography variant='h6'>OpenJira</Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import type { NextPage } from 'next';
 import { Typography } from '@mui/material';
+import { Layout } from '../components/layouts';
 
 const HomePage: NextPage = () => {
   return (
-    <>
+    <Layout>
       <Typography variant='h1' color='primary'>
         Hello Next
       </Typography>
-    </>
+    </Layout>
   );
 };
 
